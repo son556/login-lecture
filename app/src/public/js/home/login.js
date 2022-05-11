ba839492e4f04fc2b4ea0422a18cbe6e1ddd2ed6
son556 <shhmun02@naver.com>
2022-05-11 15:47:21 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -13,4 +13,12 @@ function login() {
         psword : psword.value,
     };
     console.log(req);
+    console.log(JSON.stringify(req));
+    fetch("/login",  {
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body : JSON.stringify(req),
+    });
 }
