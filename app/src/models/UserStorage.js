5cf5a64636558326fe0e92f430299e783794a219
son556 <shhmun02@naver.com>
2022-05-12 17:13:41 +0900

    회원가입페이지 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,17 +1,17 @@
 "use strict";
 
-
 class UserStorage {
-     static #users = {// static -> class 자체에서 변수를 받아올 때 정적 변수로 만들어야 가져올 수 있음
+    static #users = {// static -> class 자체에서 변수를 받아올 때 정적 변수로 만들어야 가져올 수 있음
         // #변수 -> 변수를 public 에서 private 변수로 바꿔줌 (외부에서 접근을 못하도록 함)
-        id : ["woorimIT", "나개발", "김팀장"],
-        psword : ["1234", "1234", "123456"],
-        name : ["우리밋", "나개발", "김팀장"],
-    };
+        id: ["woorimIT", "나개발", "김팀장"],
+        psword: ["1234", "1234", "123456"],
+        name: ["우리밋", "나개발", "김팀장"],
+    }
+
     static getUsers(...fields) {
         const users = this.#users
         const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)){
+            if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
             }
             return newUsers;
@@ -26,7 +26,7 @@ class UserStorage {
         const userInfo = usersKeys.reduce((newUsers, info) => {
             newUsers[info] = users[info][idx];
             return newUsers;
-        },{});
+        }, {});
 
         return userInfo;
     }
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.crtl");
 
 router.get("/", ctrl.output.hello);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 module.exports = router;
