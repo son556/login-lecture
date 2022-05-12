ea3908556dd9ac9292c9386142fc5c3c5e24fc82
son556 <shhmun02@naver.com>
2022-05-12 10:36:54 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,8 +12,7 @@ function login() {
         id : id.value,
         psword : psword.value,
     };
-    console.log(req);
-    console.log(JSON.stringify(req));
+    
     fetch("/login",  {
         method: "POST",
         headers:{
@@ -22,5 +21,14 @@ function login() {
         body : JSON.stringify(req),
     })
         .then((res) => res.json())
-        .then(console.log);
+        .then((res) => {
+            if (res.success) {
+                location.href = "/";
+            } else {
+                alert(res.msg);
+            };
+        })
+        .catch((err) => {
+            console.error(new Error("로그인 중 에러 발생"));
+        });
 }
